87994c6585488a789dd2311e83aca5135f08b3e8
Raul Fuentes <ra.fuentes.sam@gmail>
2016-02-01 16:20:42 +0100

    Original testing programs

---
+++ examples/ipv6/CoAP-IDS/IPv6_HC06_TinyDTLS/coaps-client.c
@@ -0,0 +1,206 @@
+/*
+ * Copyright (c) 2013, Institute for Pervasive Computing, ETH Zurich
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of the Institute nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE INSTITUTE AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL THE INSTITUTE OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
+ *
+ * This file is part of the Contiki operating system.
+ */
+
+/**
+ * \file
+ *      Erbium (Er) CoAPs) client example.
+ * \author
+ * 	Raul Fuentes  <ra.fuentess.sam+coaps@gmail.com>
+ *      Matthias Kovatsch <kovatsch@inf.ethz.ch>
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "contiki.h"
+#include "contiki-net.h"
+#include "er-coap-engine.h"
+#include "dev/button-sensor.h"
+
+//#include "tinydtls.h"
+//#include "dtls.h"
+
+#if DEBUG
+#include <stdio.h>
+#define PRINTF(...) printf(__VA_ARGS__)
+#define PRINT6ADDR(addr) PRINTF("[%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x:%02x%02x]", ((uint8_t *)addr)[0], ((uint8_t *)addr)[1], ((uint8_t *)addr)[2], ((uint8_t *)addr)[3], ((uint8_t *)addr)[4], ((uint8_t *)addr)[5], ((uint8_t *)addr)[6], ((uint8_t *)addr)[7], ((uint8_t *)addr)[8], ((uint8_t *)addr)[9], ((uint8_t *)addr)[10], ((uint8_t *)addr)[11], ((uint8_t *)addr)[12], ((uint8_t *)addr)[13], ((uint8_t *)addr)[14], ((uint8_t *)addr)[15])
+#define PRINTLLADDR(lladdr) PRINTF("[%02x:%02x:%02x:%02x:%02x:%02x]", (lladdr)->addr[0], (lladdr)->addr[1], (lladdr)->addr[2], (lladdr)->addr[3], (lladdr)->addr[4], (lladdr)->addr[5])
+#else
+#define PRINTF(...)
+#define PRINT6ADDR(addr)
+#define PRINTLLADDR(addr)
+#endif
+
+/* FIXME: This server address is hard-coded for Cooja and link-local for unconnected border router. */
+#define SERVER_NODE(ipaddr)   uip_ip6addr(ipaddr, 0xfe80, 0, 0, 0, 0x0212, 0x7402, 0x0002, 0x0202)      /* cooja2 */
+/* #define SERVER_NODE(ipaddr)   uip_ip6addr(ipaddr, 0xbbbb, 0, 0, 0, 0, 0, 0, 0x1) */
+
+//Definidos en contiki/core/*udp*
+#define LOCAL_PORT      UIP_HTONS(COAP_DEFAULT_PORT + 1)
+#define REMOTE_PORT     UIP_HTONS(COAP_DEFAULT_PORT)
+
+#define TOGGLE_INTERVAL 10
+
+
+/* VARIABLES */
+/* The ECDSA pair of keys are required for the clients. 
+ * We must conclude a way to populate the keys (or just using PSK ? )
+ * But for this Thesis will be the most unsecure way to do it.
+ */
+static const unsigned char ecdsa_priv_key[] = {
+			0x41, 0xC1, 0xCB, 0x6B, 0x51, 0x24, 0x7A, 0x14,
+			0x43, 0x21, 0x43, 0x5B, 0x7A, 0x80, 0xE7, 0x14,
+			0x89, 0x6A, 0x33, 0xBB, 0xAD, 0x72, 0x94, 0xCA,
+			0x40, 0x14, 0x55, 0xA1, 0x94, 0xA9, 0x49, 0xFA};
+
+static const unsigned char ecdsa_pub_key_x[] = {
+			0x36, 0xDF, 0xE2, 0xC6, 0xF9, 0xF2, 0xED, 0x29,
+			0xDA, 0x0A, 0x9A, 0x8F, 0x62, 0x68, 0x4E, 0x91,
+			0x63, 0x75, 0xBA, 0x10, 0x30, 0x0C, 0x28, 0xC5,
+			0xE4, 0x7C, 0xFB, 0xF2, 0x5F, 0xA5, 0x8F, 0x52};
+
+static const unsigned char ecdsa_pub_key_y[] = {
+			0x71, 0xA0, 0xD4, 0xFC, 0xDE, 0x1A, 0xB8, 0x78,
+			0x5A, 0x3C, 0x78, 0x69, 0x35, 0xA7, 0xCF, 0xAB,
+			0xE9, 0x3F, 0x98, 0x72, 0x09, 0xDA, 0xED, 0x0B,
+			0x4F, 0xAB, 0xC3, 0x6F, 0xC7, 0x72, 0xF8, 0x29};
+
+PROCESS(er_example_client, "HC06 CoAPS");
+AUTOSTART_PROCESSES(&er_example_client);
+
+uip_ipaddr_t server_ipaddr;
+static struct etimer et;
+
+/* Example URIs that can be queried. */
+#define NUMBER_OF_URLS 4
+/* leading and ending slashes only for demo purposes, get cropped automatically when setting the Uri-Path */
+char *service_urls[NUMBER_OF_URLS] =
+{ ".well-known/core", "./actuators/toggle", "battery/", "error/in//path" };
+#if PLATFORM_HAS_BUTTON
+static int uri_switch = 0;
+#endif
+
+
+
+
+/* This function is will be passed to COAP_BLOCKING_REQUEST() to handle responses. */
+void
+client_chunk_handler(void *response)
+{
+  const uint8_t *chunk;
+
+  int len = coap_get_payload(response, &chunk);
+
+  printf("|%.*s", len, (char *)chunk);
+}
+PROCESS_THREAD(er_example_client, ev, data)
+{
+  PROCESS_BEGIN();
+
+  static coap_packet_t request[1];      /* This way the packet can be treated as pointer as usual. */
+
+  SERVER_NODE(&server_ipaddr);
+
+  /* receives all CoAP messages */
+  coap_init_engine();
+
+  etimer_set(&et, TOGGLE_INTERVAL * CLOCK_SECOND);
+
+  /* TinyDTLS implementation */
+  static session_t dst;
+  dtls_init();
+  init_dtls(&dst);
+  
+    if (!dtls_context) {
+    dtls_emerg("cannot create context\n");
+     printf("--DTLS SE MURIO!!! Adios mundo cruel!--\n");
+    PROCESS_EXIT();
+  }
+  
+#if PLATFORM_HAS_BUTTON
+  SENSORS_ACTIVATE(button_sensor);
+  printf("Press a button to request %s\n", service_urls[uri_switch]);
+#endif
+
+  while(1) {
+    PROCESS_YIELD();
+
+    if(etimer_expired(&et)) {
+      printf("--Toggle timer--\n");
+
+      /* prepare request, TID is set by COAP_BLOCKING_REQUEST() */
+      coap_init_message(request, COAP_TYPE_CON, COAP_POST, 0);
+      coap_set_header_uri_path(request, service_urls[1]);
+
+      const char msg[] = "Toggle!";
+
+      coap_set_payload(request, (uint8_t *)msg, sizeof(msg) - 1);
+
+      PRINT6ADDR(&server_ipaddr);
+      PRINTF(" : %u\n", UIP_HTONS(REMOTE_PORT));
+
+      COAP_BLOCKING_REQUEST(&server_ipaddr, REMOTE_PORT, request,
+                            client_chunk_handler);
+
+      printf("\n--Done--\n");
+
+      etimer_reset(&et);
+
+#if PLATFORM_HAS_BUTTON
+    } else if(ev == sensors_event && data == &button_sensor) {
+
+      /* send a request to notify the end of the process */
+
+      coap_init_message(request, COAP_TYPE_CON, COAP_GET, 0);
+      coap_set_header_uri_path(request, service_urls[uri_switch]);
+
+      printf("--Requesting %s--\n", service_urls[uri_switch]);
+
+      PRINT6ADDR(&server_ipaddr);
+      PRINTF(" : %u\n", UIP_HTONS(REMOTE_PORT));
+
+      /* Hay de dos o esto lo metemos en un  "hilo"  "funcion" de TinyDTLS 
+         o a TinyDTLS lo metemos en el er-coap
+         Si considereamos que esto debería ser en arquitectura de layer ...*/
+      COAP_BLOCKING_REQUEST(&server_ipaddr, REMOTE_PORT, request,
+                            client_chunk_handler);
+
+      printf("\n--Done--\n");
+
+      uri_switch = (uri_switch + 1) % NUMBER_OF_URLS;
+#endif
+    }
+  }
+
+  PROCESS_END();
+}
+
